d534780e66755f7b2c45c9a83c3260f0d817fd11
Shinkaiiiiiiii <bryanteugenio108@gmail.com>
2025-09-20 11:39:59 +0800

    Initial site files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Starter script
+const question = document.getElementById('question');
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+const stickers = document.getElementById('stickers');
+
+// Placeholders for later logic
+console.log('Script loaded');
